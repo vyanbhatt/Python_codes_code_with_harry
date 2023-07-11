5a9d89e8536b30d4295a097acf3e1633de774379
Vasu Bhatt <bhattvasu3@gmail.com>
2023-07-11 12:20:10 +0530

    String_dive_deep

--- string_dive_deep.py
+++ string_dive_deep.py
@@ -1,10 +1,14 @@
-print("hello world")
-a=60
-b=60
-print(a+b)
+name1="vasu bhatt"
+name2='bholo'
+#It allows string between single and double quotes both
 
-def fun1():
-    #This is function
-    print(a)
+print(name1)
+print(name2)
 
-fun1()
+#how to slice the string
+print(name1[0:2])#starts from 0 and goes till 1
+print(name1[0:4])#starts from 0 and goes till 3
+
+#String functions
+print(name1.capitalize())#converts the string in capital
+print(name2.count('bh'))#counts occurences of substring bh in given string
