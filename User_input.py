e5aa067ebe6a37e1d2d6a3f5ac99c71ed2d23bb4
Vasu Bhatt <bhattvasu3@gmail.com>
2023-07-11 12:35:28 +0530

    User_input

---
+++ User_input.py
@@ -0,0 +1,14 @@
+name = input()
+print(name)
+
+#we can also give promtps in input functions
+name2= input("Enter your second name : ")
+print(name2)
+
+#Imp->Input function pupulates/converts our variable into string
+num=input("enter a number ")
+print(int(num) + 6) #since by default num var is string we had to do type cast
+
+
+#we can also mention multiple arguments in print function
+print(name,name2,num)
