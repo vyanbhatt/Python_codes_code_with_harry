a3e34249b88c1e04fe54e7b70a10ad1ab5a4bbf3
Vasu Bhatt <bhattvasu3@gmail.com>
2023-07-12 11:54:31 +0530

    if_else

---
+++ if_else.py
@@ -0,0 +1,19 @@
+'''a = input("enter a : ")
+b = input("enter b : ")
+
+if(a>b):
+    print("han bhai a moto ch")
+else:
+    print("nana ho bhai aama b moto ch")
+'''
+#implementing age varification program
+
+age = int(input("Enter your age : "))
+
+if(age > 18):
+    print("You are elligible for marriage")
+
+elif(age == 18):
+    print("Wait for one more year champ")
+else:
+    print("You're too young to get married boy")
