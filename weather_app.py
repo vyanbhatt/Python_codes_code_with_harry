b199145ec1f42aa424440d45153c2ce7e28ad14f
Vasu Bhatt <bhattvasu3@gmail.com>
2023-07-13 23:35:55 +0530

    Weather_app text to voice added

--- weather_app.py
+++ weather_app.py
@@ -1,6 +1,9 @@
 import requests #this helps to get things from net
 import json #this helps to convert string into dictionary
+import pyttsx3 #for text to speech
+
 city = input("Enter the name of the city\n")
+
 
 url = f"http://api.weatherapi.com/v1/current.json?key=b7df91ea474e4da498c173942231307&q={city}"
 
@@ -19,12 +22,30 @@ called json'''
 wdic = json.loads(r.text)
 #loads will convert string to dictionary
 
+
+
 print(wdic["location"]["name"])
 print(wdic["location"]["region"])
 print(wdic["location"]["country"])
 print(wdic["location"]["localtime"])
 
-print("Following is the temperature ")
-print(wdic["current"]["temp_c"])
+x = "Following is the temperature in celcius"
+
+# Initialize the speech synthesizer
+engine = pyttsx3.init()
+        
+# Convert text to speech
+engine.say(x)
+engine.runAndWait()
+
+x = wdic["current"]["temp_c"]
+
+# Initialize the speech synthesizer
+engine = pyttsx3.init()
+        
+# Convert text to speech
+engine.say(x)
+engine.runAndWait()
+
 
 
